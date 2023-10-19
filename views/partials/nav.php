df6169ca97fbab1f5794a007d495b46d352d5e7d
RagySamehW <ragy2106213@miuegypt.edu.eg>
2023-10-19 15:01:18 +0300

    csss

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,80 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesheet" href="../public/css/nav.css">
-    <nav class="wus-menu">
-	
-        <div class="wus-menu__button">
-             
+<link rel="stylesheet" href="../public/css/nav.css">
+<nav class="wus-menu">
+
+    <div class="wus-menu__button">
+
         <div class="wus-burger-icon">
-          <div class="wus-burger-bar"></div>
-          <div class="wus-burger-bar"></div>
-          <div class="wus-burger-bar"></div>
+            <div class="wus-burger-bar"></div>
+            <div class="wus-burger-bar"></div>
+            <div class="wus-burger-bar"></div>
         </div>
-            
-        </div>
-        
-        <div class="wus-menu__close"></div>
-            
-        
-        <div class="wus-menu__content">
-                
+
+    </div>
+
+    <div class="wus-menu__close"></div>
+
+
+    <div class="wus-menu__content">
+
         <div class="wus-nav-container">
-                
-              <div class="quick-links">
-              <a target="_blank" href="/about/term-dates/"><button style="width:145px" class="white-outline">Term Dates &rarr;</button></a>
-            <!--  <a target="_blank" href="/school-life/media/"><button style="width:130px"class="white-outline">Stories &rarr;</button></a>-->
-                    <a target="_blank" href="https://intranet.westminster.org.uk/security/external/index.asp"><button style="width:145px" class="white-outline">Intranets &rarr;</button></a>
-                    </div>
-        
-            
-        <ul class="wus-nav">
-            
-               <li class="wus-nav-item">
-               <span class="wus-nav-item__title"><a class="wus-nav-item__title-link" href="/">Home</a></span>
-            </li>
-        
-            <li class="wus-nav-item">
-                    <span class="wus-nav-item__title">About</span>
-                    
+
+            <div class="quick-links">
+                <a target="_blank" href="/about/term-dates/"><button style="width:145px" class="white-outline">Term Dates &rarr;</button></a>
+                <!--  <a target="_blank" href="/school-life/media/"><button style="width:130px"class="white-outline">Stories &rarr;</button></a>-->
+                <a target="_blank" href="https://intranet.westminster.org.uk/security/external/index.asp"><button style="width:145px" class="white-outline">Intranets &rarr;</button></a>
+            </div>
+
+
+            <ul class="wus-nav">
+
+                <li class="wus-nav-item">
+                    <span class="wus-nav-item__title"><a class="wus-nav-item__title-link" href="/">Home</a></span>
                 </li>
-                        <li class="wus-nav-item">
-                        <span class="wus-nav-item__title">Admissions</span>
-                    
-            </li>
-                            <li class="wus-nav-item">
-                        <span class="wus-nav-item__title">Academic Life</span>
-                
-            </li>
-                            <li class="wus-nav-item">
-                        <span class="wus-nav-item__title">School Life</span>
-                    
-            </li>
-            <li class="wus-nav-item">
-                        <span class="wus-nav-item__title">Co-Curriculum</span>
-                
-            </li>
-            <li class="wus-nav-item">
-                        <span class="wus-nav-item__title">Partnerships</span>
-                
-            </li>
-            
-        <a target="_blank" href="https://www.facebook.com/WestminsterSchoolUK/">
-            <i class="fab fa-facebook fa-lg" aria-hidden="true"></i></a>
-        <a target="_blank" href="https://twitter.com/wschool">
-            <i class="fab fa-twitter fa-lg" aria-hidden="true"></i></a>
-        <a target="_blank" href="https://www.instagram.com/westminstergram__/">
-            <i class="fab fa-instagram fa-lg" aria-hidden="true"></i></a>
-        <a target="_blank" href="https://www.linkedin.com/company/westminster-school-london">
-            <i class="fab fa-linkedin-in fa-lg" aria-hidden="true"></i></a>
-        <a target="_blank" href="/website-search/">
-            <i class="fa-regular fa-magnifying-glass fa-lg" aria-hidden="true"></i></a>
-            
-          </ul>  
-        
-        
+
+                <li class="wus-nav-item">
+                    <span class="wus-nav-item__title">About</span>
+
+                </li>
+                <li class="wus-nav-item">
+                    <span class="wus-nav-item__title">Admissions</span>
+
+                </li>
+                <li class="wus-nav-item">
+                    <span class="wus-nav-item__title">Academic Life</span>
+
+                </li>
+                <li class="wus-nav-item">
+                    <span class="wus-nav-item__title">School Life</span>
+
+                </li>
+                <li class="wus-nav-item">
+                    <span class="wus-nav-item__title">Co-Curriculum</span>
+
+                </li>
+                <li class="wus-nav-item">
+                    <span class="wus-nav-item__title">Partnerships</span>
+
+                </li>
+
+                <a target="_blank" href="https://www.facebook.com/WestminsterSchoolUK/">
+                    <i class="fab fa-facebook fa-lg" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://twitter.com/wschool">
+                    <i class="fab fa-twitter fa-lg" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://www.instagram.com/westminstergram__/">
+                    <i class="fab fa-instagram fa-lg" aria-hidden="true"></i></a>
+                <a target="_blank" href="https://www.linkedin.com/company/westminster-school-london">
+                    <i class="fab fa-linkedin-in fa-lg" aria-hidden="true"></i></a>
+                <a target="_blank" href="/website-search/">
+                    <i class="fa-regular fa-magnifying-glass fa-lg" aria-hidden="true"></i></a>
+
+            </ul>
+
+
         </div>
-                
-        </div>
-        
-         </nav>
+
+    </div>
+
+</nav>
+
 </html>
