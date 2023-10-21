cdb4e75f7709ca990f4f3afc835a990ad4e15877
RagySamehW <ragy2106213@miuegypt.edu.eg>
2023-10-21 16:28:27 +0300

    attendance

---
+++ views/attendance.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Styled Form</title>
+    <?php
+    include "./partials/nav1.html"
+    ?>
+    <style>
+        /* Reset some default browser styles */
+        body,
+        h1,
+        p,
+        form,
+        button {
+            margin: 0;
+            padding: 0;
+        }
+
+        /* Global styles */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            color: #333;
+        }
+
+        .container1 {
+            max-width: 500px;
+            margin: 0 auto;
+            margin-top: 100px;
+            margin-bottom: 115px;
+            padding: 20px;
+            background-color: #fff;
+            border: 1px solid #e1e1e1;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
+        h1 {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        form {
+            text-align: left;
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            display: block;
+            font-weight: 600;
+        }
+
+        .form-group {
+            display: flex;
+            align-items: center;
+        }
+
+        .label-left,
+        .label-right {
+            display: inline;
+            margin: 0;
+            padding: 0;
+        }
+
+        .label-left {
+            margin-right: 305px;
+            /* Adjust this margin to control the space between labels */
+        }
+
+        button {
+            background-color: #3498db;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            padding: 10px 20px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+            margin-left: 175px;
+        }
+
+
+        button:hover {
+            background-color: #2980b9;
+        }
+
+        /* Form animation */
+        .form-group {
+            animation: fadeIn 1s ease-in-out;
+        }
+
+        @keyframes fadeIn {
+            0% {
+                opacity: 0;
+                transform: translateY(10px);
+            }
+
+            100% {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+
+        .label-text {
+            margin: 0;
+            padding: 0;
+            width: 350px;
+            /* Set the width of the label */
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container1">
+        <h1>
+            <?php
+            echo date("l, F j, Y") . "<br>";
+            ?>
+        </h1>
+        <form>
+            <div class="form-group">
+                <label for="name" style="color: red;" class="label-left">Name</label>
+                <label id="name" style="color: red;" class="label-right" name="name">Attendance</label>
+            </div>
+            <div class="form-group">
+                <label for="subscribe" class="label-text">Karen Osama</label>
+                <input type="checkbox" id="subscribe" name="subscribe" checked>
+            </div>
+            <div class="form-group">
+                <label for="subscribe" class="label-text">Nour Hesham</label>
+                <input type="checkbox" id="subscribe" name="subscribe" checked>
+            </div>
+            <div class="form-group">
+                <label for="subscribe" class="label-text">Farah Mohamed</label>
+                <input type="checkbox" id="subscribe" name="subscribe" checked>
+            </div>
+            <div class="form-group">
+                <label for="subscribe" class="label-text">Abdo Halawa</label>
+                <input type="checkbox" id="subscribe" name="subscribe" checked>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</body>
+
+</html>
+<?php
+include "./partials/footer.html"
+?>
