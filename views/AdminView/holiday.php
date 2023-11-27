95e5033508a3ce4d02aff6ccbeec2b4b5926a331
farah <you@example.com>
2023-11-27 20:09:07 +0200

    edits in admindashboard

---
+++ views/AdminView/holiday.php
@@ -0,0 +1,198 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+<title>Preskool - Holiday</title>
+
+<link rel="shortcut icon" href="assets/img/favicon.png">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/plugins/feather/feather.css">
+
+<link rel="stylesheet" href="assets/plugins/icons/flags/flags.css">
+
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+<link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<?php
+include "adminNav.php";
+?>
+
+
+
+
+
+<div class="page-wrapper">
+<div class="content container-fluid">
+
+<div class="page-header">
+<div class="row align-items-center">
+<div class="col">
+<h3 class="page-title">Holiday</h3>
+<ul class="breadcrumb">
+<li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+<li class="breadcrumb-item active">Holiday</li>
+</ul>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="card card-table">
+<div class="card-body">
+
+<div class="page-header">
+<div class="row align-items-center">
+<div class="col">
+<h3 class="page-title">Holiday</h3>
+</div>
+<div class="col-auto text-end float-end ms-auto download-grp">
+<a href="#" class="btn btn-outline-primary me-2"><i class="fas fa-download"></i> Download</a>
+<a href="add-holiday.html" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+</div>
+</div>
+</div>
+
+<div class="table-responsive">
+<table class="table border-0 star-student table-hover table-center mb-0 datatable table-striped">
+<thead class="student-thread">
+<tr>
+<th>ID</th>
+<th>Holiday Name</th>
+<th>Type</th>
+<th>Start Date</th>
+<th>End Date</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>PRE2209</td>
+<td>
+<h2>
+<a>Sports Day</a>
+</h2>
+</td>
+<td>College Holiday</td>
+<td>17 Aug 2020</td>
+<td>19 Aug 2020</td>
+</tr>
+<tr>
+<td>PRE2213</td>
+<td>
+<h2>
+<a>Memorial Day</a>
+</h2>
+</td>
+<td>Public Holiday</td>
+<td>05 Aug 2020</td>
+<td>06 Aug 2020</td>
+</tr>
+<tr>
+<td>PRE2143</td>
+<td>
+<h2>
+<a>Annual Day</a>
+</h2>
+</td>
+<td>College Holiday</td>
+<td>04 Sept 2020</td>
+<td>07 Sept 2020</td>
+</tr>
+<tr>
+<td>PRE2431</td>
+<td>
+<h2>
+<a>Exam Holiday</a>
+</h2>
+</td>
+<td>Semester leave</td>
+<td>17 Sept 2020</td>
+<td>30 Sept 2020</td>
+</tr>
+<tr>
+<td>PRE2209</td>
+<td>
+<h2>
+<a>Sports Day</a>
+</h2>
+</td>
+<td>College Holiday</td>
+<td>17 Aug 2020</td>
+<td>19 Aug 2020</td>
+</tr>
+<tr>
+<td>PRE2213</td>
+<td>
+<h2>
+<a>Memorial Day</a>
+</h2>
+</td>
+<td>Public Holiday</td>
+<td>05 Aug 2020</td>
+<td>06 Aug 2020</td>
+</tr>
+<tr>
+<td>PRE2143</td>
+<td>
+<h2>
+<a>Annual Day</a>
+</h2>
+</td>
+<td>College Holiday</td>
+<td>04 Sept 2020</td>
+<td>07 Sept 2020</td>
+</tr>
+<tr>
+<td>PRE2431</td>
+<td>
+<h2>
+<a>Exam Holiday</a>
+</h2>
+</td>
+<td>Semester leave</td>
+<td>17 Sept 2020</td>
+<td>30 Sept 2020</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<footer>
+<p>Copyright © 2022 Dreamguys.</p>
+</footer>
+
+</div>
+
+</div>
+
+
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/js/feather.min.js"></script>
+
+<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<script src="assets/plugins/datatables/datatables.min.js"></script>
+
+<script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ views/AdminView/schoolfees.php
@@ -0,0 +1,329 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+<title>Preskool - Fees</title>
+
+<link rel="shortcut icon" href="assets/img/favicon.png">
+
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="assets/plugins/feather/feather.css">
+
+<link rel="stylesheet" href="assets/plugins/icons/flags/flags.css">
+
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+<link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
+
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+
+<body>
+
+<?php
+include "adminNav.php";
+?>
+
+
+<div class="page-wrapper">
+<div class="content container-fluid">
+
+<div class="page-header">
+<div class="row align-items-center">
+<div class="col">
+<h3 class="page-title">Fees</h3>
+<ul class="breadcrumb">
+<li class="breadcrumb-item"><a href="adminindex.php">Dashboard</a></li>
+<li class="breadcrumb-item active">Fees</li>
+</ul>
+</div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-12">
+<div class="card card-table">
+<div class="card-body">
+
+<div class="page-header">
+<div class="row align-items-center">
+<div class="col">
+<h3 class="page-title">Fees</h3>
+</div>
+<div class="col-auto text-end float-end ms-auto download-grp">
+<a href="#" class="btn btn-outline-primary me-2"><i class="fas fa-download"></i> Download</a>
+<a href="add-fees.html" class="btn btn-primary"><i class="fas fa-plus"></i></a>
+</div>
+</div>
+</div>
+
+<div class="table-responsive">
+<table class="table border-0 star-student table-hover table-center mb-0 datatable table-striped">
+<thead class="student-thread">
+<tr>
+<th>ID</th>
+<th>Fees Name</th>
+<th>Class</th>
+<th>Amount</th>
+<th>Start Date</th>
+<th>End Date</th>
+<th class="text-end">Action</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>PRE2209</td>
+<td>
+<h2>
+<a>Exam Fees</a>
+</h2>
+</td>
+<td>10</td>
+<td>$345</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE2213</td>
+<td>
+<h2>
+<a>Exam Fees</a>
+</h2>
+</td>
+<td>1</td>
+<td>$255</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+ </tr>
+<tr>
+<td>PRE2143</td>
+<td>
+<h2>
+<a>Exam Fees</a>
+</h2>
+</td>
+<td>9</td>
+<td>$545</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE2431</td>
+<td>
+<h2>
+<a>Sports Day Fees</a>
+</h2>
+</td>
+<td>8</td>
+<td>$234</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE1534</td>
+<td>
+<h2>
+<a>Exam Fees</a>
+</h2>
+</td>
+<td>7</td>
+<td>$265</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE2153</td>
+<td>
+<h2>
+<a>Sports Day Fees</a>
+</h2>
+</td>
+<td>2</td>
+<td>$334</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE1434</td>
+<td>
+<h2>
+<a>Sports Day Fees</a>
+</h2>
+</td>
+<td>6</td>
+<td>$341</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE2345</td>
+<td>
+<h2>
+<a>Exam Fees</a>
+</h2>
+</td>
+<td>12</td>
+<td>$365</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+ <a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE2365</td>
+<td>
+<h2>
+<a>Annual Day Fees</a>
+</h2>
+</td>
+<td>11</td>
+<td>$83</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+<tr>
+<td>PRE1234</td>
+<td>
+<h2>
+<a>Class Test Fees</a>
+</h2>
+</td>
+<td>5</td>
+<td>$242</td>
+<td>23 Apr 2020</td>
+<td>28 Apr 2020</td>
+<td class="text-end">
+<div class="actions">
+<a href="javascript:;" class="btn btn-sm bg-success-light me-2">
+<i class="feather-eye"></i>
+</a>
+<a href="edit-fees.html" class="btn btn-sm bg-danger-light">
+<i class="feather-edit"></i>
+</a>
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<footer>
+<p>Copyright © 2022 Dreamguys.</p>
+</footer>
+
+</div>
+
+</div>
+
+
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/js/feather.min.js"></script>
+
+<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+
+<script src="assets/plugins/datatables/datatables.min.js"></script>
+
+<script src="assets/js/script.js"></script>
+</body>
+</html>
