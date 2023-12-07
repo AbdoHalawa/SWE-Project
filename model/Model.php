2c00765830cd78917c850dab5032cfda6615d491
nourhesham116 <nourhesham116@gmail.com>
2023-12-07 18:09:44 +0200

    working on (ADD) For student and parent

--- model/Model.php
+++ model/Model.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
-require_once(__ROOT__ ."/Db/Dbh.php");
-abstract class Model{
+define('__ROOT__', dirname(dirname(__FILE__)));
+
+require_once(__ROOT__ . "/Db/Dbh.php");
+
+abstract class Model
+{
     protected $db;
     protected $conn;
 
@@ -10,11 +14,57 @@ abstract class Model{
         $this->db = $this->connect();
     }
 
-    public function connect(){
-        if(null === $this->conn ){
+    public function connect()
+    {
+        if (null === $this->conn) {
             $this->db = new Dbh();
         }
         return $this->db;
+    }
+
+    // Common method to execute SQL queries
+    protected function executeQuery($sql, $params = [])
+    {
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+
+    // Select records from the database
+    protected function select($table, $columns = "*", $where = "", $params = [])
+    {
+        $sql = "SELECT $columns FROM $table";
+        if (!empty($where)) {
+            $sql .= " WHERE $where";
+        }
+
+        $stmt = $this->executeQuery($sql, $params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // Insert a record into the database
+    protected function insert($table, $data)
+    {
+        $columns = implode(", ", array_keys($data));
+        $values = ":" . implode(", :", array_keys($data));
+
+        $sql = "INSERT INTO $table ($columns) VALUES ($values)";
+        $this->executeQuery($sql, $data);
+    }
+
+    // Update records in the database
+    protected function update($table, $data, $where, $params = [])
+    {
+        $setClause = implode(" = ?, ", array_keys($data)) . " = ?";
+        $sql = "UPDATE $table SET $setClause WHERE $where";
+        $this->executeQuery($sql, array_merge(array_values($data), $params));
+    }
+
+    // Delete records from the database
+    protected function delete($table, $where, $params = [])
+    {
+        $sql = "DELETE FROM $table WHERE $where";
+        $this->executeQuery($sql, $params);
     }
 }
 ?>
---
+++ model/StudentModel.php
@@ -0,0 +1,39 @@
+<?php
+require_once(__DIR__ . '/Model.php');
+
+class StudentModel extends Model
+{
+    public function insertStudent($student)
+    {
+        // Your logic to insert a student into the database
+        $sql = "INSERT INTO Students (FirstName, LastName, Gender, DateOfBirth, Religion, Grade, ClassID, AdmissionID, ParentID, PhoneNumber, Email, Password)
+                VALUES (
+                    '{$student->getFirstName()}',
+                    '{$student->getLastName()}',
+                    '{$student->getGender()}',
+                    '{$student->getDateOfBirth()}',
+                    '{$student->getReligion()}',
+                    '{$student->getGrade()}',
+                    '{$student->getClassID()}',
+                    '{$student->getAdmissionID()}',
+                    '{$student->getParentID()}',
+                    '{$student->getPhoneNumber()}',
+                    '{$student->getEmail()}',
+                    '{$student->getPassword()}'
+                )";
+
+        $result = $this->db->query($sql);
+
+        if ($result) {
+            echo $sql;
+            // Insert successful
+            return true;
+        } else {
+            echo "Error: " . $this->db->getConn()->error;
+            return false;
+        }
+    }
+
+    // Add other methods as needed
+}
+?>
